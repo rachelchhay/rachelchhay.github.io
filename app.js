0b4262fd1833eddf8b7ad7e430a1d5e4323a4046
Rachel Chhay <rachelchhay@Rachels-MacBook-Pro.local>
2017-07-18 11:49:42 -0500

    Set up menu, objs in JS, and arrays in JS

--- app.js
+++ app.js
@@ -1,7 +1,38 @@
+const foodArray = ["cake", "blueberry", "strawberry", "cupcake", "vanilla frosting", "chocolate frosting", "coffee", "smoothie", "hamburger", "hot dog", "ketchup", "lettuce"];
+
+
+
+
+
+const customer1 = {
+  happiness: true,
+  orderArr: ["blueberry", "cake", "coffee"]
+};
+
+const player = {
+  orderCompletedArr: []
+
+
+};
+
+// If orderCompletedArr equals orderArr, customer's happiness=True
+
+
+
+
+// windows onload
 $(() => {
 
 
-alert("testing");
+$('li').addClass('menu-items');
+
+$('.menu-items').on('click', (e) => {
+
+  
+})
 
 
+
+
+// windows onload end
 })
