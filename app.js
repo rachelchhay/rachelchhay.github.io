5f7be2c3ad90c1d4cc49354accb230cfb463dadc
Rachel Chhay <rachelchhay@Rachels-MacBook-Pro.local>
2017-07-18 12:52:26 -0500

    checkOrder() works

--- app.js
+++ app.js
@@ -1,16 +1,46 @@
-const foodArray = ["cake", "blueberry", "strawberry", "cupcake", "vanilla frosting", "chocolate frosting", "coffee", "smoothie", "hamburger", "hot dog", "ketchup", "lettuce"];
+// windows onload
+$(() => {
+
+
+// const foodArray = ["cake", "blueberry", "strawberry", "cupcake", "vanilla frosting", "chocolate frosting", "coffee", "smoothie", "hamburger", "hot dog", "ketchup", "lettuce"];
 
 
 
 
 
 const customer1 = {
-  happiness: true,
-  orderArr: ["blueberry", "cake", "coffee"]
+  happiness: false,
+  orderArr: ["blueberry", "cake", "coffee"],
+  payForOrder() {
+    if(this.happiness === true) {
+      player.coins++;
+      player.xp++;
+    }
+  }
 };
 
 const player = {
-  orderCompletedArr: []
+  coins: 0,
+  xp: 0,
+  orderCompletedArr: [],
+  checkOrder() {
+    // check if two arrays match
+    if(this.orderCompletedArr.length === customer1.orderArr.length) {
+      for(let i = 0; i < customer1.orderArr.length; i++) {
+         if(customer1.orderArr[i] !== this.orderCompletedArr[i]) {
+           customer1.happiness = false;
+           console.log("arrays don't match");
+           return false; //ends loop
+         }
+      }
+      customer1.happiness = true;
+      console.log("Items match");
+    } else {
+      customer1.happiness = false;
+      console.log("Arrays are different lengths");
+    }
+
+  }
 
 
 };
@@ -20,16 +50,24 @@ const player = {
 
 
 
-// windows onload
-$(() => {
 
 
 $('li').addClass('menu-items');
 
 $('.menu-items').on('click', (e) => {
+  const menuText = $(e.currentTarget).text().toLowerCase();
+  player.orderCompletedArr.push(menuText);
+  console.log(player.orderCompletedArr);
+});
 
-  
-})
+// order finished button
+
+$('button').on('click', (e) => {
+  player.checkOrder();
+  console.log(customer1.happiness);
+});
+
+
 
 
 
