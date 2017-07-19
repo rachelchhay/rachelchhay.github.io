ca8260aeb260c45a08872afce683291404a40fc2
Rachel Chhay <rachelchhay@Rachels-MacBook-Pro.local>
2017-07-18 22:05:10 -0500

    customer order is appended to the page

--- app.js
+++ app.js
@@ -8,9 +8,15 @@ $(() => {
 
 
 
-const customer1 = {
+const customer = {
   happiness: false,
   orderArr: ["blueberry", "cake", "coffee"],
+  customerOrder() {
+    for(let i = 0; i < this.orderArr.length; i++) {
+      const order = $('<li/>').html(this.orderArr[i]);
+      $('.customer').append(order);
+    }
+  },
   payForOrder() {
     if(this.happiness === true) {
       player.coins++;
@@ -19,24 +25,26 @@ const customer1 = {
   }
 };
 
+customer.customerOrder();
+
 const player = {
   coins: 0,
   xp: 0,
   orderCompletedArr: [],
   checkOrder() {
     // check if two arrays match
-    if(this.orderCompletedArr.length === customer1.orderArr.length) {
-      for(let i = 0; i < customer1.orderArr.length; i++) {
-         if(customer1.orderArr[i] !== this.orderCompletedArr[i]) {
-           customer1.happiness = false;
+    if(this.orderCompletedArr.length === customer.orderArr.length) {
+      for(let i = 0; i < customer.orderArr.length; i++) {
+         if(customer.orderArr[i] !== this.orderCompletedArr[i]) {
+           customer.happiness = false;
            console.log("arrays don't match");
            return false; //ends loop
          }
       }
-      customer1.happiness = true;
+      customer.happiness = true;
       console.log("Items match");
     } else {
-      customer1.happiness = false;
+      customer.happiness = false;
       console.log("Arrays are different lengths");
     }
 
@@ -51,7 +59,7 @@ const player = {
 
 
 
-$('li').addClass('menu-items');
+$('#menu li').addClass('menu-items');
 
 $('.menu-items').on('click', (e) => {
   const menuText = $(e.currentTarget).text().toLowerCase();
@@ -63,8 +71,8 @@ $('.menu-items').on('click', (e) => {
 
 $('button').on('click', (e) => {
   player.checkOrder();
-  console.log(customer1.happiness);
-  customer1.payForOrder();
+  console.log(customer.happiness);
+  customer.payForOrder();
   console.log("Player coins: " + player.coins);
   console.log("Player xp: " + player.xp);
 
