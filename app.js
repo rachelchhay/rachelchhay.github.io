d1f8d0560594ff8ab5c4a7b2bebc6c8b89756fde
Rachel Chhay <rachelchhay@Rachels-MacBook-Pro.local>
2017-07-20 10:14:31 -0500

    Next round button can be clicked, new menu items work

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ $(() => {
 
 
 // Array of food on menu
-const menu = ["hamburger", "sandwich", "pizza", "coffee", "milkshake", "cupcake", "pie", "hot dog", "fries", "salad", "soda", "cake", "ice cream"];
+const menu = [];
+// const menu = ["hamburger", "sandwich", "pizza", "coffee", "milkshake", "cupcake", "pie", "hot dog", "fries", "salad", "soda", "cake", "ice cream"];
 
 
 // customer object========================================
@@ -57,9 +58,11 @@ const player = {
     // check if two arrays match
     if(this.orderCompletedArr.length === customer.orderArr.length) {
       for(let i = 0; i < customer.orderArr.length; i++) {
-         if(customer.orderArr[i] !== this.orderCompletedArr[i]) {
+         if(customer.orderArr[i].toLowerCase() !== this.orderCompletedArr[i]) {
            customer.happiness = false;
            console.log("arrays don't match");
+           console.log(this.orderCompletedArr);
+           console.log(customer.orderArr);
            return false; //ends loop
          }
       }
@@ -84,17 +87,14 @@ const nextOff = () => {
 
 
 // start game round 1 ======================================
-let round = 0;
 
 const start = () => {
-  round = 1;
-  if(round === 1) {
     customer.randomFood(1);
     // customer.randomFood(Math.floor(Math.random() * (5 - 3)) + 3);
-  }
+
   customer.customerOrder();
 }
-start();
+// start();
 
 // round 2 class ========================================
 
@@ -149,54 +149,34 @@ $('#next').on('click', (e) => {
   start();
   nextOff();
 
-  if(player.coins === 5) {
+  if(player.coins === 2) {
     const round2 = prompt("You have enough coins to go on to level 2! Would you like to keep going?", "yes/no");
     if(round2 === "yes" || round2 === "Yes" || round2 === "y") {
-      alert("Click the Next Round button");
+      // alert("Click the Next Round button");
       $('body').append($('<button>').text("Next Round").attr('id', 'next-round'));
+
+      $('#next-round').on('click', (e) => {
+        console.log("clicked next round");
+      });
+
     }
   }
 });
 
-$('#next-round').on('click', (e) => {
-  console.log("clicked next round");
-  // secondRound.addMenuItem("Waffles", "Fried Chicken", "Spaghetti");
-  // secondRound.changeCustomersLeft(8);
-  // secondRound.changeRandomFood(Math.floor(Math.random() * (7 - 3)) + 3);
-});
 
 
+// secondRound.addMenuItem("Waffles", "Fried Chicken", "Spaghetti");
+// secondRound.changeCustomersLeft(8);
+// secondRound.changeRandomFood(Math.floor(Math.random() * (7 - 3)) + 3);
 
 
-// New menu items aren't being recognized as right
-// Next Round button not working
 // Math.random argument isn't changing
 
 
 
 
+start();
 
 
 // windows onload end
 })
-
-
-// const roundTwo = () => {
-//   round = 2;
-//   if(player.coins === 5) {
-//     const round2 = prompt("You have enough coins to go on to level 2! Would you like to keep going?", "yes/no");
-//     if(round2 === "yes" || round2 === "Yes" || round2 === "y") {
-//       menu.push("waffles", "fried chicken", "spaghetti");
-//       $('#menu').append($('<li>').addClass('menu-items').text("Waffles"));
-//       $('#menu').append($('<li>').addClass('menu-items').text("Fried Chicken"));
-//       $('#menu').append($('<li>').addClass('menu-items').text("Spaghetti"));
-//       player.customersLeft = 8;
-//       $('#customers-left').text('Customers Left: ' + player.customersLeft);
-//       if(round === 2) {
-//         customer.randomFood(Math.floor(Math.random() * (7 - 3)) + 3);
-//       }
-//     } else {
-//       alert("Come back and play Rachel's Restaurant Rush!")
-//     }
-//   }
-// };
