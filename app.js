bc27519d7fb57f052bf4ab54d73a2ac810f5ad4b
Rachel Chhay <rachelchhay@Rachels-MacBook-Pro.local>
2017-07-23 23:22:51 -0500

    Cleaned up code, changed arguments

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ const customer = {
 const player = {
   coins: 0,
   xp: 0,
-  customersLeft: 5,
+  customersLeft: 4,
   orderCompletedArr: [],
   checkOrder() {
     // check if two arrays match
@@ -61,8 +61,6 @@ const player = {
          if(customer.orderArr[i].toLowerCase() !== this.orderCompletedArr[i]) {
            customer.happiness = false;
            console.log("arrays don't match");
-           console.log(this.orderCompletedArr);
-           console.log(customer.orderArr);
            return false; //ends loop
          }
       }
@@ -71,8 +69,6 @@ const player = {
     } else {
       customer.happiness = false;
       console.log("Arrays are different lengths");
-      console.log(this.orderCompletedArr);
-      console.log(customer.orderArr);
     }
   }
 
@@ -87,8 +83,7 @@ const nextOff = () => {
 // start game round 1 ======================================
 let round = 0;
 const start = () => {
-  customer.randomFood(1 + round);
-    // customer.randomFood(Math.floor(Math.random() * (5 - 3)) + 3);
+  customer.randomFood((Math.floor(Math.random() * (4 - 2)) + 3) + round);
 
   customer.customerOrder();
 }
@@ -96,49 +91,46 @@ start();
 
 // round class ========================================
 
+const newMenuItem = (str) => {
+  $('#menu').append($('<li>').addClass('menu-items').text(str).on('click', (e) => {
+    $('#buttonAudio')[0].play();
+    const menuText = $(e.currentTarget).text().toLowerCase();
+    player.orderCompletedArr.push(menuText);
+    const orderReady = $('<li/>').html($(e.currentTarget).text());
+    $('.player').append(orderReady);
+  }));
+}
+
 class Rounds {
   addMenuItem (str1, str2, str3) {
     menu.push(str1, str2, str3);
-    $('#menu').append($('<li>').addClass('menu-items').text(str1).on('click', (e) => {
-      const menuText = $(e.currentTarget).text().toLowerCase();
-      player.orderCompletedArr.push(menuText);
-      console.log(player.orderCompletedArr);
-    }));
-    $('#menu').append($('<li>').addClass('menu-items').text(str2).on('click', (e) => {
-      const menuText = $(e.currentTarget).text().toLowerCase();
-      player.orderCompletedArr.push(menuText);
-      console.log(player.orderCompletedArr);
-    }));
-    $('#menu').append($('<li>').addClass('menu-items').text(str3).on('click', (e) => {
-      const menuText = $(e.currentTarget).text().toLowerCase();
-      player.orderCompletedArr.push(menuText);
-      console.log(player.orderCompletedArr);
-    }));
+    newMenuItem(str1);
+    newMenuItem(str2);
+    newMenuItem(str3);
   }
   changeCustomersLeft (num) {
     player.customersLeft = num;
     $('#customers-left').text('Customers Left: ' + player.customersLeft);
   }
-
 };
 
 const secondRound = new Rounds();
 const thirdRound = new Rounds();
 
 // Set Timer Function =======================================
-let timeLeft = 10;
+let timeLeft = 90;
 let startInterval;
 const timer = () => {
   $('#game-status').append($('<li>').attr('id', 'timer'));
   startInterval = setInterval( () => {
     $('#timer').text("Timer: " + timeLeft);
-    if(timeLeft <= 5) {
+    if(timeLeft <= 10) {
       $('#timer').toggleClass('blink');
     };
     if(timeLeft === 0) {
       clearInterval(startInterval);
     };
-    if(player.coins < 18 && timeLeft === 0) {
+    if(player.coins < 15 && timeLeft === 0) {
       setTimeout( function () {alert("You lose. You ran out of time.")}, 1000);
       setTimeout( function () {location.reload(true)}, 2000);
     };
@@ -155,7 +147,6 @@ $('.menu-items').on('click', (e) => {
   $('#buttonAudio')[0].play();
   const menuText = $(e.currentTarget).text().toLowerCase();
   player.orderCompletedArr.push(menuText);
-  console.log(player.orderCompletedArr);
   const orderReady = $('<li/>').html($(e.currentTarget).text());
   $('.player').append(orderReady);
 
@@ -166,15 +157,12 @@ $('#order-up').on('click', (e) => {
   $('#orderUpAudio')[0].play();
   player.checkOrder();
   customer.payForOrder();
-  console.log("Player coins: " + player.coins);
-  console.log("Player xp: " + player.xp);
-  console.log("Customers left: " + player.customersLeft);
 
   // win or lose the game
   if(player.coins <= 0 || player.xp <= 0) {
     setTimeout( function () {alert("You lose :(")}, 1000);
     setTimeout( function () {location.reload(true)}, 2000);
-  } else if(player.coins === 7) {
+  } else if(player.coins === 18) {
     setTimeout( function () {alert("You win!")}, 1000);
     setTimeout( function () {location.reload(true)}, 2000);
   };
@@ -193,7 +181,7 @@ $('#next').on('click', (e) => {
     }
 
   // ROUND 2 ====================================
-    if(player.coins === 2) {
+    if(player.coins === 4) {
       round = 2;
       const round2 = prompt("You have enough coins to go on to level 2! Would you like to keep going?", "yes/no");
       if(round2 === "yes" || round2 === "Yes" || round2 === "y") {
@@ -206,9 +194,8 @@ $('#next').on('click', (e) => {
         $('#next-round').on('click', (e) => {
           $('#buttonAudio')[0].play();
           timer();
-          console.log("clicked next round");
           secondRound.addMenuItem("Waffles", "Tater Tots", "Spaghetti");
-          secondRound.changeCustomersLeft(6);
+          secondRound.changeCustomersLeft(5);
           customer.customerOrder();
 
           $('#next-round').off('click');
@@ -221,7 +208,7 @@ $('#next').on('click', (e) => {
     }
 
   // ROUND 3 ====================================
-    if(player.coins === 5) {
+    if(player.coins === 9) {
       clearInterval(startInterval);
       round = 3;
       const round2 = prompt("You have enough coins to go on to level 3! Would you like to keep going?", "yes/no");
@@ -231,11 +218,10 @@ $('#next').on('click', (e) => {
         $('.customer li').remove();
 
         $('#next-round').on('click', (e) => {
-          timeLeft = 50;
+          timeLeft = 70;
           timer();
-          console.log("clicked next round");
           thirdRound.addMenuItem("Pancakes", "Corn Dog", "Cookies");
-          thirdRound.changeCustomersLeft(7);
+          thirdRound.changeCustomersLeft(6);
           customer.customerOrder();
           $('#next-round').off('click');
         });
@@ -243,10 +229,7 @@ $('#next').on('click', (e) => {
         setTimeout( function () {alert("Come back when you want to play!")}, 1000);
         setTimeout( function () {location.reload(true)}, 2000);
       }
-
     }
-
-
 });
 
 
